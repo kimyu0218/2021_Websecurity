d38cd9612db0566d009298dd25c8114195c9c8a3
juhyojeong <juhyojeong58@gmail.com>
2021-12-06 18:06:54 +0900

    Add: backend files

---
+++ app.py
@@ -0,0 +1,91 @@
+import os
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+app.run(host='0.0.0.0', debug = True)
+
+@app.route('/pdf_parsing', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.form
+        for file in files:
+            print('--')
+            print(file)
+        print(files.to_dict(flat=False))
+        for file in files.to_dict(flat=False)['files']:
+            file.save('/var/www/html/files/'+secure_filename(file.filename))
+
+        os.system("ls -d /var/www/html/files/* > ~/pdfs.txt && cd ~/hidost/build/ && ./src/cacher -i ~/pdfs.txt --compact --values -c cache/ -t10 -m256 && find cache -name '*.pdf' -not -empty > cached-pdfs.txt && ./src/pathcount -i cached-pdfs.txt -o pathcounts.bin && ./src/feat-select -i pathcounts.bin -o features.nppf -m1000 && ./src/feat-extract cached-pdfs.txt -f features.nppf --values -o data.libsvm")
+
+        f = open("./feature_count.txt", "r")
+        feature_line = int(f.read())
+        f.close()
+
+        input_f = open('./data.libsvm', 'r')
+        output_f = open('./output.csv', 'w')
+
+        lines = input_f.readlines()
+        for line in lines:
+            minus_list = [-1] * (feature_line - 1)
+            
+            l = line.split(' #')[0]
+            l = l.split(' ')
+            file_name = line.split(' #')[1].strip('\n')
+            
+            for i in range(len(l)):
+                if i != 0:
+                    key = int(l[i].split(':')[0])
+                    val = float(l[i].split(':')[1])
+                    minus_list[key-1]=val
+            if int(l[0]) == 1:
+                mb = 'M'
+            else:
+                mb = 'B'
+            
+            result_string=mb+', '+str(minus_list).strip('[]')+', '+file_name
+            output_f.write(result_string)
+        input_f.close()
+        output_f.close()
+
+        return "string"
+
+
+@app.route('/load_model')
+def load_model():
+    data = pd.read_csv("./output.csv", header=None, error_bad_lines=False)
+
+    data = data.drop(data.columns[-1], axis=1) # 필요 없는 데이터 제거 (파일의 맨 마지막에 파일 경로 저장)
+    data_drop = data.drop(0, axis=1) # 파일 라벨 제거
+    Y = data[0] # 0번째 컬럼에 라벨 저장되어 있음
+
+    datas = pd.DataFrame(data_drop.iloc[:,0:])
+    datas.columns = data_drop.iloc[:,0:].columns
+
+    X = datas.values
+
+    f = open("./upload_count.txt", "r")
+    upload_count = int(f.read())
+    f.close()
+
+    x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=upload_count/len(X),random_state=0)
+    y_train = y_train.to_numpy()
+    y_test = y_test.to_numpy() 
+
+    rfc = RandomForestClassifier(random_state=0)
+    rfc.fit(x_train, y_train)
+
+    predict = rfc.predict(x_test)
+    score = accuracy_score(y_test, predict)
+    print("Acc: " + str(score))
+    print("Predict: " + str(predict))
+
+    return predict.tolist()
